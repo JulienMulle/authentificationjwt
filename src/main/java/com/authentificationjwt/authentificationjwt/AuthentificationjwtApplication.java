0e9f288b2722beb379de482c35049fae2b7f60e4
julien <julienmulle@gmail.com>
2022-08-09 16:45:25 +0200

    first commit

---
+++ src/main/java/com/authentificationjwt/authentificationjwt/AuthentificationjwtApplication.java
@@ -0,0 +1,13 @@
+package com.authentificationjwt.authentificationjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthentificationjwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthentificationjwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/authentificationjwt/authentificationjwt/AuthentificationjwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.authentificationjwt.authentificationjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthentificationjwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
